5fc32d57e86834a8d5acb31873c93b102448a1b2
Wilmar Osorio Usuga <wilmar.osorio1@udea.edu.co>
2022-07-20 17:42:01 -0500

    se agregan las nuevas clases

---
+++ main/java/Pokemon/Pokebola.java
@@ -0,0 +1,21 @@
+
+package Pokemon;
+
+/**
+ *
+ * @author WilmarOS
+ */
+public class Pokebola {
+    
+    private String Tamaño;
+    private pokemon;            /*no entiendo el atributo */
+    
+    public String getTamaño(){
+        return Tamaño;
+    }
+    
+    public pokemon getPokemon(){
+        return Pokemon;
+    }
+}
+
---
+++ main/java/Pokemon/Squirtle.java
@@ -0,0 +1,21 @@
+
+package Pokemon;
+
+/**
+ *
+ * @author WilmarOS
+ */
+public class Squirtle extends Pokemon{
+    
+    private String nombre;
+    private Byte nivel;
+    private int salud;
+
+    public Squirtle(String nombre, Byte nivel, int salud) {
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.salud = salud;
+    }
+    
+    
+}
