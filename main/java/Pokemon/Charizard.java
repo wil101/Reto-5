310e71ef956dfc3e9c1cc6a7a0d616bf8f4bd6f5
Wilmar Osorio Usuga <wilmar.osorio1@udea.edu.co>
2022-07-20 15:55:53 -0500

    prueba en conexion

---
+++ main/java/Pokemon/Charizard.java
@@ -0,0 +1,19 @@
+package Pokemon;
+
+/**
+ * @author emanuel.lopezh
+ */
+public class Charizard extends Charmeleon {
+
+    @Override
+    public Pokemon evolucionar() {
+        return null;
+    }
+
+    @Override
+    public String gritar() {
+        return "Charizard!";
+    }
+    
+    
+}
---
+++ main/java/Pokemon/Main.java
@@ -0,0 +1,10 @@
+package Pokemon;
+/**
+ *
+ * @author emanuel.lopezh
+ */
+public class Main {
+    public static void main(String[] args){
+        Charmander pokemon1 = new Charmander("WIlmar", 12, 10);
+    }
+}
